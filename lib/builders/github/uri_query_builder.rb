b84b3c9a061db4e77b31dbb64110bcbf731e4dbb
Ramon Lopes <ramonlopes.87@gmail.com>
2020-08-15 21:06:30 -0300

    Adds Github::UriQueryBuilder
    This builder will receive the parameters to be used in the search for github repositories. It will return a query string formatted according to the descriptor standards in the github doc ('q' values separated by '+')

---
+++ lib/builders/github/uri_query_builder.rb
@@ -0,0 +1,51 @@
+module Builders
+  module Github
+    class UriQueryBuilder
+      private_class_method :new
+
+      def self.build(params)
+        new(params).build_uri_query
+      end
+
+      def initialize(params)
+        @keywords = params[:q]
+        @username = params[:username]
+        @language = params[:language] || 'ruby'
+        @sort = params[:sort] || 'stars'
+        @order = params[:order] || 'desc'
+      end
+
+      def build_uri_query
+        {
+          q: formatted_query,
+          sort: sort,
+          order: order
+        }.map{ |k, v| "#{k}=#{v}" }.join("&")
+      end
+
+      private
+
+      attr_reader :keywords, :username, :language, :sort, :order
+
+      def formatted_query
+        [
+          keywords,
+          formatted_language,
+          formatted_username
+        ].compact.join('+')
+      end
+
+      def formatted_language
+        return nil unless language.present?
+
+        "language:#{language}"
+      end
+
+      def formatted_username
+        return nil unless username.present?
+
+        "user:#{username}"
+      end
+    end
+  end
+end
